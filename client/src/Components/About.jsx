57408ac66ed419f4cd33e9f99ee1fea5502cc029
Musa Sayar <musasayar67@gmail.com>
2024-02-29 16:17:58 +0300

    user is controlled navbar

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -2,9 +2,19 @@
 const About = () => {
   return (
     <div>
-      
+      Çok faktörlü kimlik doğrulma (MFA) bir bilgisayar kullanıcısına,
+      kullanıcının kimlik doğrulama mekanizmasına başarılı bir şekilde iki ya da
+      daha fazla kanıt sağladığında erişim sağlandığı bir kimlik doğrulama
+      yöntemidir: bilgi (sadece ve sadece kullanıcının bildiği bir şey),
+      sahiplik (sadece ve sadece kullanıcının sahip olduğu bir şey), ve devralma
+      (sadece ve sadece kullanıcının olduğu bir şey).[1][2] İki faktörlü kimlik
+      doğrulama (2FA olarak da bilinir) çoklu kimlik doğrulamasının bir çeşidi
+      ya da alt grubudur. Kullanıcıların sahip oldukları kimliklerini iki farklı
+      faktörün kombinasyonunu kullanarak onaylama yöntemidir. 1 Kullanıcının
+      bildiği bir şey, 2 Kullanıcının sahip olduğu bir şey, ya da 3
+      Kullanıcının olduğu bir şey.
     </div>
-  )
+  );
 }
 
 export default About
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
-
+import {useSelector} from "react-redux"
 const Navbar = () => {
+const {user} = useSelector((state)=>state.user)
+ 
   return (
     <nav className="bg-white dark:bg-gray-900   z-20 top-0 start-0  border-b border-gray-300 dark:border-gray-600 shadow ">
       <div className="max-w-6xl flex flex-wrap items-center justify-between mx-auto p-3">
         <h2 className="text-lg md:text-xl font-bold text-slate-600 ">
-         Auth{" "}
-          <span className="text-emerald-500 font-bold">OTP</span>
+          Auth <span className="text-emerald-500 font-bold">OTP</span>
         </h2>
         <div className="flex gap-2 font-semibold text-md ">
           <NavLink
@@ -25,32 +26,54 @@ const Navbar = () => {
           >
             About
           </NavLink>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => {
-              return isActive ? "text-teal-600   " : "text-[#334155] ";
-            }}
-          >
-            Profile
-          </NavLink>
+
+          {user && user.verified && (
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => {
+                return isActive ? "text-teal-600   " : "text-[#334155] ";
+              }}
+            >
+              Profile
+            </NavLink>
+          )}
         </div>
         <div className="font-semibold text-sm md:text-md bg-white px-4 py-2 border border-gray-300 rounded-sm shadow-md ">
           {/* <button className="px-2   rounded-md">
             <Link className="">Register</Link>
           </button> */}
 
-          <button>
-            <NavLink
-              to="/sign-in"
-              className={({ isActive }) => {
-                return isActive
-                  ? "text-teal-600    "
-                  : "text-[#334155] hover:text-teal-600 ";
-              }}
-            >
-              Sign In
-            </NavLink>
-          </button>
+          {user && user?.verified ? (
+            <>
+              <button>
+                <NavLink
+                  to="/sign-in"
+                  className={({ isActive }) => {
+                    return isActive
+                      ? "text-teal-600    "
+                      : "text-[#334155] hover:text-teal-600 ";
+                  }}
+                >
+                  Sign Out
+                </NavLink>
+              </button>
+            </>
+          ) : (
+            <>
+              <button>
+                <NavLink
+                  to="/sign-in"
+                  className={({ isActive }) => {
+                    return isActive
+                      ? "text-teal-600    "
+                      : "text-[#334155] hover:text-teal-600 ";
+                  }}
+                >
+                  Sign İn
+                </NavLink>
+              </button>
+            </>
+          )}
         </div>
       </div>
     </nav>
